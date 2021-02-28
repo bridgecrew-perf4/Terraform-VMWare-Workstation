4212a7c4a8a712827e91d92ba9e3a05f813278ea
ej <ej@nextresearch.io>
2021-02-28 07:50:05 -0500

    fix script lines2

--- doInTerminal2.sh
+++ doInTerminal2.sh
@@ -6,8 +6,11 @@ export VMWS_USER="ejbest"; \
 export VMWS_PASSWORD="tT!666666"; \
 export VMWS_URL="https://localhost:8697/api"; \
 terraform init
-ansible-vault decrypt terraform/vault/vmw.tfvars; \
-terraform plan -state=terraform/envi/vmw/terraform.tfstate -var-file=terraform/vault/vmw.tfvars terraform/envi/vmw/; \
-ansible-vault encrypt terraform/vault/vmw.tfvars
-# 
+#ansible-vault decrypt terraform/vault/vmw.tfvars; \
+#terraform plan -state=terraform/envi/vmw/terraform.tfstate -var-file=terraform/vault/vmw.tfvars terraform/envi/vmw/; \
+#ansible-vault encrypt terraform/vault/vmw.tfvars
+#
+terraform init
+terraform plan 
+terraform apply --auto-approve 
 # end of job
