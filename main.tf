f4d138e34f4fa3bb367da21f0a03e435886fa3a7
ej <ej@nextresearch.io>
2021-03-03 08:20:15 -0500

    build attempt adjust vars

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
       vmworkstation = {
       source        = "elsudano/vmworkstation"
-      version       = "0.1.7"
+      version       = "0.1.9"
     }
   }
 }
